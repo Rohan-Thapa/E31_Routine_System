ef8796e1c91278b472f0f45db3f6c5670e61c4e3
Rohan Thapa <rohanthapa632@gmail.com>
2022-02-18 16:37:29 +0545

    Create Routine_System.py

---
+++ Routine_System.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from tkinter import ttk, messagebox
+from functools import partial
+
+classes = [
+    'ST4059CEM Legal and Ethical Foundations in Cyber Security',
+    'ST4061CEM Programming and Algorithm 1 - Batch 30',
+    'ST4065CEM Computer System & Networks',
+    'Quiz'
+]
+
+options = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+]
